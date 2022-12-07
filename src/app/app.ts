694eb90993bbe13e8c3204ff17c99ed5fc5d8415
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-12-07 13:37:40 +0300

    feat: добавлена страница 500

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { renderLayoutCentered } from "shared/ui/layouts/centered/centered";
 
-import { chatPage, loginPage, page_404, registerPage } from "../pages";
+import { chatPage, loginPage, page_404, page_500, registerPage } from "../pages";
 
 import "./styles/index.scss";
 
@@ -9,6 +9,7 @@ const pages: { [K: string]: { title: string; component: string } } = {
   "/register": { title: "Регистрация", component: registerPage },
   "/chat": { title: "Чат", component: chatPage },
   "/404": { title: "404", component: page_404 },
+  "/500": { title: "500", component: page_500 },
 };
 
 const currentRoute = location.pathname;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 import { page_404 } from "./404/404";
+import { page_500 } from "./500/500";
 import { chatPage } from "./chat/chat";
 import { loginPage } from "./login/login";
 import { registerPage } from "./register/register";
 
-export { chatPage, loginPage, page_404, registerPage };
+export { chatPage, loginPage, page_404, page_500, registerPage };

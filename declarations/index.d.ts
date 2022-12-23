51f81933f44591009f88338b451f456ef1036519
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-12-23 17:40:33 +0300

    refactor: types

--- declarations/index.d.ts
+++ declarations/index.d.ts
@@ -1,28 +1,32 @@
-declare module "*.hbs" {
-  const source: string;
-  export default source;
+declare global {
+  export module "*.hbs" {
+    const source: string;
+    export default source;
+  }
+
+  export module "*.svg" {
+    const source: string;
+    export default source;
+  }
+
+  export module "*.jpg" {
+    const source: string;
+    export default source;
+  }
+
+  export type TRenderContext = { [K: string]: string | boolean };
+
+  export type TRenderProps = {
+    context?: TRenderContext;
+  };
+
+  export type TRender = (source: string, context?: TRenderContext, options?: CompileOptions) => string;
+
+  export type TRenderCreator = <T = TRenderContext>(
+    source: string,
+    defaultContext?: T,
+    options?: CompileOptions
+  ) => (context?: T) => string;
 }
 
-declare module "*.svg" {
-  const source: string;
-  export default source;
-}
-
-declare module "*.jpg" {
-  const source: string;
-  export default source;
-}
-
-declare type TRenderContext = { [K: string]: string | boolean };
-
-declare type TRenderProps = {
-  context?: TRenderContext;
-};
-
-declare type TRender = (source: string, context?: TRenderContext, options?: CompileOptions) => string;
-
-declare type TRenderCreator = <T = TRenderContext>(
-  source: string,
-  defaultContext?: T,
-  options?: CompileOptions
-) => (context?: T) => string;
+export {};

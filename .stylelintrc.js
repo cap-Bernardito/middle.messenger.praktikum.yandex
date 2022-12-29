f931c6d34a9682417fa9967576ecd25b18f12bca
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2022-12-29 16:12:19 +0300

    chore: переименован файл .stylelintrc.js чтобы CI проходили

--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-clean-order", "stylelint-config-prettier"],
-  plugins: ["stylelint-order", "stylelint-scss"],
-  rules: {
-    "selector-class-pattern":
-      "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$",
-  },
-  ignoreFiles: ["dist/*", "node_modules/*"],
-};

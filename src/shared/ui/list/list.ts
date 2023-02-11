8bf689d9c64b3951569c9fc5b31d86b4dd95beef
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-02-11 09:55:43 +0300

    refactor: code

--- src/shared/ui/list/list.ts
+++ src/shared/ui/list/list.ts
@@ -1,15 +1,10 @@
-import { TChatState } from "pages/messenger/chat/model/store";
-
 import { Block } from "shared/core";
 
 import "./list.scss";
 
 export type TListProps = {
-  items: (Block | string | Record<string, unknown>)[];
+  items: (Block | string)[];
   className?: string;
-  itemTemplate?: BlockConstructable;
-  itemPropsMap?: Record<string, string>;
-  chatData?: TChatState["chatData"];
 };
 
 export class List extends Block<TListProps> {
@@ -18,33 +13,11 @@ export class List extends Block<TListProps> {
   constructor({ items, ...props }: TListProps) {
     super({
       ...props,
-      items: items,
+      items,
     });
   }
 
   render() {
-    const { items, itemTemplate, itemPropsMap, chatData } = this.props;
-
-    if (items && itemTemplate && itemPropsMap && chatData) {
-      const itemMappedProps = (data: Record<string, unknown>) => {
-        const result = Object.entries(itemPropsMap).map(([key, value]) => `${key}="${data[value]}"`);
-
-        result.push(`chatId="${chatData.id}"`);
-
-        return result.join(" ");
-      };
-
-      const itemsStringify = items
-        .map((item) => `{{{ ${itemTemplate.cName} ${itemMappedProps(item as Record<string, unknown>)} }}}`)
-        .join("");
-
-      return `
-      <ul class="list {{{className}}}">
-        ${itemsStringify}
-      </ul>
-          `;
-    }
-
     return `
 <ul class="list {{{className}}}">
   {{#each items}}
--- src/widgets/list-with-users/list-with-users.ts
+++ src/widgets/list-with-users/list-with-users.ts
@@ -1,19 +1,54 @@
+import { TChatState } from "pages/messenger/chat/model/store";
+
 import { List, TListProps } from "shared/ui";
 import { connect } from "shared/utils/connect";
 
 const withChatUsers = connect((state) => {
   return {
-    items: state.chat.users,
+    itemsData: state.chat.users,
     chatData: state.chat.chatData,
     user: state.auth.user,
   };
 });
 
+type TListWithUsers = TListProps & {
+  itemsData: Record<string, unknown>[];
+  itemTemplate?: BlockConstructable;
+  itemPropsMap?: Record<string, string>;
+  chatData?: TChatState["chatData"];
+};
+
 export const ListWithUsers = withChatUsers(
   // @ts-ignore
   class extends List {
-    constructor(props: TListProps) {
+    constructor(props: TListWithUsers) {
       super(props);
+    }
+
+    render() {
+      const { itemsData, itemTemplate, itemPropsMap, chatData } = this.props as TListWithUsers;
+
+      if (itemsData && itemTemplate && itemPropsMap && chatData) {
+        const itemMappedProps = (data: Record<string, unknown>) => {
+          const result = Object.entries(itemPropsMap).map(([key, value]) => `${key}="${data[value]}"`);
+
+          result.push(`chatId="${chatData.id}"`);
+
+          return result.join(" ");
+        };
+
+        const itemsStringify = itemsData
+          .map((item) => `{{{ ${itemTemplate.cName} ${itemMappedProps(item as Record<string, unknown>)} }}}`)
+          .join("");
+
+        return `
+        <ul class="list {{{className}}}">
+          ${itemsStringify}
+        </ul>
+            `;
+      }
+
+      return super.render();
     }
   }
 );
